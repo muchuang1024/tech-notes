b90171fb807b34b16c935c3226f4438f39698ea8
caijinlin <caijinlin2012@gmail.com>
2023-06-04 22:18:39 +0800

    MOD 介绍

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -36,58 +36,96 @@ module.exports = {
       {
         text: '工具',
         items: [
-            {
-                text: '效率提升', 
-                items: [
-                    {text: 'Autojump', link: '/blog/工具/效率提升/autojump/'},
-                    {text: 'Karabiner', link: '/blog/工具/效率提升/karabiner-Elements/'},
-                    {text: 'KeyCastr', link: '/blog/工具/效率提升/KeyCastr/'},
-                    {text: 'Item2', link: '/blog/工具/效率提升/item2/'},
-                    {text: 'Zsh', link: '/blog/工具/效率提升/zsh/'},
-                    {text: 'Tmux', link: '/blog/工具/效率提升/tmux/'},
-                ]
-            },
-            {
-                text: '编程开发', 
-                items: [
-                    {text: 'Mac', link: '/blog/工具/编程开发/mac/'},
-                    {text: 'Linux', link: '/blog/工具/编程开发/linux/'},
-                    {text: 'Vim', link: '/blog/工具/编程开发/vim/'},
-                    {text: 'Git', link: '/blog/工具/编程开发/git/'},
-                    {text: 'Sublime', link: '/blog/工具/编程开发/sublime/'},
-                ]
-            },
-            {
-                text: '通用软件', 
-                items: [
-                    {text: 'Typora', link: '/blog/工具/通用软件/Typora/'},
-                    {text: 'Notion', link: '/blog/工具/通用软件/Notion/'},
-                    {text: 'Witeboard', link: '/blog/工具/通用软件/Witeboard/'},
-                    {text: '时光序', link: '/blog/工具/通用软件/时光序/'},
-                ]
-            },
-            {
-                text: '通用硬件', 
-                items: [
-                    {text: '键盘', link: '/blog/工具/通用硬件/键盘/'},
-                ]
-            }
+          {
+            text: '效率提升',
+            items: [
+              { text: 'Autojump', link: '/blog/01-工具/效率提升/autojump/' },
+              { text: 'Karabiner', link: '/blog/01-工具/效率提升/karabiner-Elements/' },
+              { text: 'KeyCastr', link: '/blog/01-工具/效率提升/KeyCastr/' },
+              { text: 'Item2', link: '/blog/01-工具/效率提升/item2/' },
+              { text: 'Zsh', link: '/blog/01-工具/效率提升/zsh/' },
+              { text: 'Tmux', link: '/blog/01-工具/效率提升/tmux/' }
+            ]
+          },
+          {
+            text: '编程开发',
+            items: [
+              { text: 'Mac', link: '/blog/01-工具/编程开发/mac/' },
+              { text: 'Linux', link: '/blog/01-工具/编程开发/linux/' },
+              { text: 'Chrome', link: '/blog/01-工具/编程开发/chrome/' },
+              { text: 'Vim', link: '/blog/01-工具/编程开发/vim/' },
+              { text: 'Git', link: '/blog/01-工具/编程开发/git/' },
+              { text: 'Sublime', link: '/blog/01-工具/编程开发/sublime/' }
+            ]
+          },
+          {
+            text: '通用软件',
+            items: [
+              { text: 'Typora', link: '/blog/01-工具/通用软件/Typora/' },
+              { text: 'Notion', link: '/blog/01-工具/通用软件/Notion/' },
+              { text: 'Witeboard', link: '/blog/01-工具/通用软件/Witeboard/' },
+              { text: '时光序', link: '/blog/01-工具/通用软件/时光序/' }
+            ]
+          },
+          {
+            text: '通用硬件',
+            items: [{ text: '键盘', link: '/blog/工具/通用硬件/键盘/' }]
+          }
         ]
+      },
+      {
+        text: 'MySQL',
+        items: [
+          { text: '基础篇', link: '/blog/05-MySQL/toc/' },
+          { text: '索引篇', link: '/blog/05-MySQL/toc/' },
+          { text: '事务篇', link: '/blog/05-MySQL/toc/' },
+          { text: '锁篇', link: '/blog/05-MySQL/toc/' },
+          { text: '日志篇', link: '/blog/05-MySQL/toc/' }
+        ]
+      },
+      {
+        text: 'Redis',
+        items: [
+          { text: '数据类型篇 ', link: '/blog/06-Redis/toc/' },
+          { text: '持久化篇', link: '/blog/06-Redis/toc/' },
+          { text: '缓存篇', link: '/blog/06-Redis/toc/' },
+          { text: '高可用篇', link: '/blog/06-Redis/toc/' }
+        ]
+      },
+      {
+        text: '操作系统',
+        link: '/blog/02-操作系统/toc/'
+      },
+      {
+        text: '计算机网络',
+        link: '/blog/03-计算机网络/toc/'
+      },
+      {
+        text: 'Golang',
+        link: '/blog/04-Golang/toc/'
+      },
+      {
+        text: '算法',
+        link: '/blog/07-算法/toc/'
+      },
+      {
+        text: '框架',
+        link: '/blog/08-框架/toc/'
+      },
+      {
+        text: '架构设计',
+        link: '/blog/09-架构设计/toc/'
       },
       {
         text: 'Github',
         link: 'https://github.com/caijinlin'
       }
     ],
-    sidebar: 'auto',
+    sidebar: 'auto'
   },
 
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-    '@vuepress/plugin-active-header-links'
-  ]
+  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom', '@vuepress/plugin-active-header-links']
 }
--- docs/src/index.md
+++ docs/src/index.md
@@ -3,13 +3,13 @@ home: true
 heroImage: https://v1.vuepress.vuejs.org/hero.png
 tagline: 在线文档网站
 actionText: Quick Start →
-actionLink: /guide/
+actionLink: /
 features:
-- title: 持续集成
-  details: 随着视频的不断更新，文档也在不断丰富，观看视频时对照文档学习效果更好。
-- title: 内容丰富
-  details: 大部分视频课程我们都提供了文档，文档内容精炼，摒弃掉啰嗦无用内容。
-- title: 每晚直播
-  details: 每晚8点直播，抖音或B站中搜索【Go程序员】
+  - title: 持续集成
+    details: 视频在不断更新，文档也在不断丰富
+  - title: 内容系统
+    details: 文档内容精炼，摒弃掉啰嗦无用内容
+  - title: 关于作者
+    details: B站、公众号、抖音中搜索【Go程序员】，加微信ck289765进交流群
 footer: Made by caspar with ❤️
 ---
